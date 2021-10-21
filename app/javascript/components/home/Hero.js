34f0733b2448076c6b09808bdd3fff49eb4c42db
Daniel <dpericich@gmail.com>
2021-10-20 22:26:07 -0500

    Stubbed out Hero component for front page

---
+++ app/javascript/components/home/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Have the hero be 2 column with left column being text of "Risk Free Investing"
+// Mitigate risks by running loss free trades on our site. Try your strategies here before investing your real money
+// The right column will be some svg or cool stock picture
+
+const Hero = () => {
+  return(
+    <div>Hello World!</div>
+  )
+};
+
+export default Hero;
