d52db03d0f650b7f8b0020be4a64e98ee0b8dc30
Daniel <dpericich@gmail.com>
2021-10-20 22:29:41 -0500

    setup App with routing for different pages and started global style file

---
+++ app/javascript/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import GlobalStyles from '../globalStyles';
+import HomePage from '../pages/HomePage';
+import ArticleIndexPage from '../pages/ArticleIndexPage'
+import AboutUsPage from '../pages/AboutUsPage';
+
+// https://www.youtube.com/watch?v=F0xErjOtJAQ -> React on Rails Tutorial - Episode 1
+
+const App = () => {
+  return(
+    <>
+      <GlobalStyles />
+      <Switch>
+        <Route path="/articles" component={ArticleIndexPage} />
+        <Route path="/about-us" component={AboutUsPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
+    </>
+  )
+}
+
+export default App;
