ff70f2db793d3ee29f04e431b94703eda4bf1ca8
Daniel <dpericich@gmail.com>
2021-11-08 23:06:55 -0600

    updated ArticlePage to use loader when getting article records from BE and to thread data into Title and ArticleText components

--- app/javascript/pages/ArticlePage.js
+++ app/javascript/pages/ArticlePage.js
@@ -1,32 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 import Header from '../components/pageBasics/Header';
 import Title from '../components/article/Title';
 import ArticleText from '../components/article/ArticleText';
 import Footer from '../components/pageBasics/Footer';
+import Loader from '../components/pageBasics/Loader';
 
 const ArticlePage = (props) => {
   const [titleInfo, setTitleInfo] = useState({})
   const [articleText, setArticleText] = useState({})
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const url = `/api/v1${props.location.pathname}`
     axios.get(url)
     .then(res => {
-      setTitleInfo({title: res.data.title})
-      setArticleText({article_text: res.data.article_text})
+      setTitleInfo({title: res.data.data.attributes.title, published_date: res.data.data.attributes.publishedAt, author: res.data.data.attributes.author});
+      setArticleText({article_text: res.data.data.attributes.articleText});
+      setLoading(false);
     })
     .catch(err => console.log(err));
   }, [])
 
-  return(
-    <>
-      <Header />
-      <Title props={titleInfo} />
-      <ArticleText props={articleText} />
-      <Footer />
-    </>
-  )
+  const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    width: 55%;
+    margin: 1rem auto;
+    padding: 0rem;
+
+    @media (max-width: 900px) {
+      width: 70%;
+      padding: .75rem 0 0;
+      margin: 0 auto;
+    }
+
+    @media (max-width: 600px) {
+      width: 85%;
+      padding: .75rem 0 0;
+      margin: 0 auto;
+    }
+  `
+
+  if (loading) {
+    return(
+      <Loader />
+    )
+  } else {
+    return(
+      <>
+        <Header />
+          <Wrapper>
+            <Title props={titleInfo} />
+            <ArticleText props={articleText} />
+          </Wrapper>
+        <Footer />
+      </>
+    )
+  }
 }
 
 export default ArticlePage;
