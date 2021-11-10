9a1becbcf54d2d3b3b5787bc332c9ff50e2c5e98
Daniel <dpericich@gmail.com>
2021-11-09 22:36:06 -0600

    added articles routes for articles_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :articles
+    end
+  end
+
   get '*path', to: 'pages#index', via: :all
 end
