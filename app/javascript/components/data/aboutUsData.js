7cb46c2f9e413c1783033aa31d1112141a26f847
Daniel <dpericich@gmail.com>
2021-11-12 21:49:01 -0600

    created aboutUsData text js file

---
+++ app/javascript/components/data/aboutUsData.js
@@ -0,0 +1,7 @@
+export const aboutUs = [
+  "PaperTrader is an online paper trading app. What is paper trading you may ask? It is the act of exchanging fake money for fake positions in a stock. This is done to be able to try, develop and refine different trading strategies without assuming the financial risk of actually trading stocks.",
+  "I have been interested in stocks and trading for a long time. At first I was hesitant to invest any money. 'After all, you can't lose money' you don't invest, but you can lose everything with a bad stock pick. Understanding that there had to be a way to invest that wasn't fraught with peril, I started looking at index funds. ",
+  "line 3",
+  "This app was built using a combination of React for the front end, Ruby on Rails for the back end and Postgresql for the database. There are a number of gems and npm packages that were also used, but I won't get to technical here.",
+  "I hope that you enjoy this app and "
+]
