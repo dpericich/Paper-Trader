aafe267a5014d60f274f85a80163a45f2d412a93
Daniel <dpericich@gmail.com>
2021-11-21 15:21:07 -0600

    ran devise migration to update users model

--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,16 @@ ActiveRecord::Schema.define(version: 2021_10_09_005833) do
     t.string "first_name"
     t.string "last_name"
     t.string "username"
+    t.string "role", default: "trader"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "articles", "users"
